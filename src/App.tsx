728351849bc954ca29c190ee01fafa1cad6e7238
Matthieu Dubo <matthieu.dubo40@gmail.com>
2021-12-09 18:54:58 +0100

    started the side bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
       <Navbar />
+      <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,25 +5,25 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__logo">
-        <div className="logo">
-          <img src={logo} alt="logo-lunetic" />
+        <img src={logo} alt="logo-lunetic" />
+      </div>
+      <div className="test">
+        <div className="navbar__menu">
+          <ul>
+            <li>Home</li>
+            <li>Concept</li>
+            <li>Opticiens</li>
+            <li>Nous</li>
+            <li>Où nous trouver</li>
+            <li>News</li>
+          </ul>
         </div>
-      </div>
-      <div className="navbar__menu">
-        <ul>
-          <li>Home</li>
-          <li>Concept</li>
-          <li>Opticiens</li>
-          <li>Nous</li>
-          <li>Où nous trouver</li>
-          <li>News</li>
-        </ul>
-      </div>
-      <div className="navbar__menu-burger">
-        <div className="navbar__box">
-          <div className="navbar__line" />
-          <div className="navbar__middle-line" />
-          <div className="navbar__line" />
+        <div className="navbar__menu-burger">
+          <div className="navbar__box">
+            <div className="navbar__line" />
+            <div className="navbar__middle-line" />
+            <div className="navbar__line" />
+          </div>
         </div>
       </div>
     </div>
