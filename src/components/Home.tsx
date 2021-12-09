10c82e68d2fbc8d208dab27fc3a77858952d8397
Matthieu Dubo <matthieu.dubo40@gmail.com>
2021-12-09 22:49:38 +0100

    started the top of the homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,32 @@
-import React from "react";
-import Sidebar from "./Sidebar";
+import React from 'react';
+import Sidebar from './Sidebar';
 
 const Home = () => {
   return (
     <div className="home">
+      <div className="home__carrousel">
+        <div className="home__text">
+          <h2>#lunetic</h2>
+          <div className="home__title">
+            <h1>
+              Hanoi Designers
+              <br />
+              Meetup
+            </h1>
+            <div className="home__underline" />
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the
+            <br />
+            printing and typesetting industry.{' '}
+            <span className="black-text">Lorem Ipsum</span>
+            <br /> has been the industry's standard dumm
+          </p>
+        </div>
+      </div>
       <Sidebar />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
