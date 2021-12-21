11ea662bde095e382cce42c390ad061851e9da23
mickacharp <mickaelcharp@gmail.com>
2021-12-21 19:55:30 +0100

    started skeleton of optician profile page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Concept from './components/Concept';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Nous from './components/Nous';
+import OpticianProfile from './components/OpticianProfile';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
           <Route path="/us" element={<Nous />} />
           <Route path="/find-us" element={<Home />} />
           <Route path="/news" element={<Home />} />
+          <Route path="/optician-profile" element={<OpticianProfile />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/OpticianProfile.tsx
+++ src/components/OpticianProfile.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
 
+import pictoGoutte from '../assets/PICTO GOUTTE_fond blanc_Plan de travail 1.png';
+
 const OpticianProfile = () => {
-  return <div className="optician-profile"></div>;
+  return (
+    <div className="optician-profile">
+      <div className="optician-profile__info">
+        <div className="optician-profile__info-img-container">
+          <img
+            src={pictoGoutte}
+            alt="profil opticien partenaire Lunetic"
+            className="optician-profile__info-img"
+          />
+        </div>
+        <hr />
+        <div className="optician-profile__address">
+          <p>
+            123 Fluffypants Lane
+            <br />
+            Pawtown, FL 32789
+            <br />
+            123-468-8901
+          </p>
+        </div>
+        <hr />
+        <div className="optician-profile__contact">
+          <p>
+            mail@mail.com
+            <br />
+            05 59 59 59 59
+            <br />
+            06 10 20 30 40
+          </p>
+        </div>
+        <hr />
+        <div className="optician-profile__administration">
+          <p>
+            Code FINESS
+            <br />
+            Siret
+            <br />
+            Num TVA
+          </p>
+        </div>
+      </div>
+
+      <div className="optician-profile__store">
+        <p className="optician-profile__close">FERMER</p>
+        <h1 className="optician-profile__store-name">Nom Magasin</h1>
+        <img
+          src={pictoGoutte}
+          alt="logo goutte Lunetic"
+          className="optician-profile__store-img"
+        />
+        <input
+          type="button"
+          value="Éditer votre profil"
+          className="optician-profile__edit"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default OpticianProfile;
