1e3cf95ea694970d42f6152dd23da62dbf680dc1
Matthieu Dubo <matthieu.dubo40@gmail.com>
2022-01-07 10:55:10 +0100

    starting develop "what is lunetic" part

---
+++ src/components/WhatIsLunetic.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WhatIsLunetic = () => {
+  return (
+    <div className="whatIsLunetic">
+      <p>
+        enjoy
+        <br />
+        your life
+      </p>
+    </div>
+  );
+};
+
+export default WhatIsLunetic;
