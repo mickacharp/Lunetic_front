577ad4ba15326515fb0d24faac43513209bb8bba
Matthieu Dubo <matthieu.dubo40@gmail.com>
2022-01-07 15:44:37 +0100

    continue home page, what is lunetic part

--- src/components/WhatIsLunetic.tsx
+++ src/components/WhatIsLunetic.tsx
@@ -3,15 +3,48 @@ import React from 'react';
 const WhatIsLunetic = () => {
   return (
     <div className="whatIsLunetic">
-      <div className="whatIsLunetic__container">
-        <p className="whatIsLunetic__p">
-          enjoy
-          <br />
-          your life
-        </p>
-        <p className="whatIsLunetic__middle">Hey! We host all events near you!</p>
+      <div className="whatIsLunetic__subtitle">
+        <div className="whatIsLunetic__container">
+          <p className="whatIsLunetic__p">
+            enjoy
+            <br />
+            your life
+          </p>
+          <p className="whatIsLunetic__middle">Hey! We host all events near you!</p>
+        </div>
+        <div className="whatIsLunetic__medium-square" />
+        <div className="whatIsLunetic__little-square" />
       </div>
-      <div className="whatIsLunetic__medium-square" />
+      <div className="whatIsLunetic__content">
+        <div className="whatIsLunetic__number-one">
+          <div className="whatIsLunetic__number">
+            <p>01</p>
+          </div>
+          <div className="whatIsLunetic__textContent">
+            <p>
+              Contrary to popular belief, Lorem Ipsum is
+              <br />
+              not simply random text. It has roots in a piece
+              <br />
+              of classical Latin literature from 45 BC,
+              <br /> making it over 2000 years old.
+            </p>
+            <div className="whatIsLunetic__btn-more">
+              <p>See More</p>
+            </div>
+          </div>
+        </div>
+        <div className="whatIsLunetic__text">
+          <p>
+            C'est
+            <br />
+            quoi
+            <br />
+            Lunetic
+            <br />?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
