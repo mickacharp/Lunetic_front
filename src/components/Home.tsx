b348df62984fa1eb18da1a10ad6450aef9f295bb
PatxiEH <patxi.hiribarren@gmail.com>
2022-01-13 14:34:35 +0100

    maps and searchbar done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -81,6 +81,7 @@ const Home = () => {
         <Sidebar />
       </div>
       <WhatIsLunetic />
+      <Maps />
     </>
   );
 };
--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,37 +1,96 @@
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import carteDepliante from '../assets/carte-depliante.png';
+import points from '../assets/cube-points-gris.png';
+import traitVertical from '../assets/trait.png';
+import cercleGris from '../assets/moyen-cercle-gris.png';
+import traitOblique from '../assets/trait-oblique-gris.png';
+import petitCercle from '../assets/petit-cercle-rouge.png';
 
 import apiKey from '../../api.js';
 import mapsStyles from '../mapsStyle';
+import SearchBarMaps from './SearchBarMaps';
 
 const containerStyle = {
   width: '100vw',
-  height: '500px',
+  height: '100%',
 };
 
-const center = {
-  lat: 43.46352270882575,
-  lng: -1.511119064793627,
-};
+interface ICenter {
+  lat: number;
+  lng: number;
+}
 
 const options = {
   styles: mapsStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
 };
 
 const Maps = () => {
+  const [zoom, setZoom] = useState(10);
+  const [center, setCenter] = useState<ICenter>({
+    lat: 43.46352270882575,
+    lng: -1.511119064793627,
+  });
+
+  const panTo: Function = (lat: number, lng: number) => {
+    setCenter({ lat, lng });
+    setZoom(13);
+  };
+
   const { isLoaded } = useJsApiLoader({
     id: 'lunetic',
     googleMapsApiKey: apiKey,
+    libraries: ['places'],
   });
 
   return isLoaded ? (
-    <>
-      <h2 className="maps__h2"> Où nous trouver</h2>
-      <div className="maps">
+    <div className="section_ou_nous_trouver">
+      <h2 className="section_ou_nous_trouver__h2"> Où nous trouver</h2>
+      <img
+        className="section_ou_nous_trouver__carte_depliante"
+        src={carteDepliante}
+        alt="carte"
+      />
+      <img className="section_ou_nous_trouver__points" src={points} alt="points" />
+      <img
+        className="section_ou_nous_trouver__trait_vertical"
+        src={traitVertical}
+        alt="trait"
+      />
+      <img
+        className="section_ou_nous_trouver__moyen_cercle_gris"
+        src={cercleGris}
+        alt="cercle-gris"
+      />
+      <img
+        className="section_ou_nous_trouver__trait_oblique"
+        src={traitOblique}
+        alt="trait-oblique"
+      />
+      <img
+        className="section_ou_nous_trouver__moyen_cercle_gris2"
+        src={cercleGris}
+        alt="cercle-gris"
+      />
+      <img
+        className="section_ou_nous_trouver__grand_trait_oblique"
+        src={traitOblique}
+        alt="trait-oblique"
+      />
+      <img
+        className="section_ou_nous_trouver__petit_cercle_rouge"
+        src={petitCercle}
+        alt="cercle-rouge"
+      />
+
+      <SearchBarMaps panTo={panTo} />
+      <div className="section_ou_nous_trouver__maps">
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={10}
+          zoom={zoom}
           options={options}>
           {/* Child components, such as markers, info windows, etc. */}
           <div>
@@ -41,6 +100,10 @@ const Maps = () => {
                   lat: 43.46352270882575,
                   lng: -1.511119064793627,
                 }}
+                /* icon={{
+                  url: '../assets/gout.png',
+                  scaledSize: new window.google.maps.Size(30, 30),
+                }} */
               />
             }
             {
@@ -53,9 +116,9 @@ const Maps = () => {
             }
           </div>
         </GoogleMap>
-        <div className="maps__child1"></div>
+        <div className="section_ou_nous_trouver__child1"></div>
       </div>
-    </>
+    </div>
   ) : (
     <></>
   );
