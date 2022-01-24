6d18e40b1acd063a3efb3d0d55ebc0b28b24d3be
mickacharp <mickaelcharp@gmail.com>
2022-01-24 17:25:58 +0100

    1st commit: creating components and scss

---
+++ src/components/ContactFormPro.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ContactFormPro = () => {
+  return <div className="contactformpro-component-container"></div>;
+};
+
+export default ContactFormPro;
---
+++ src/components/ContactFormUser.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ContactFormUser = () => {
+  return <div className="contactformuser-component-container"></div>;
+};
+
+export default ContactFormUser;
