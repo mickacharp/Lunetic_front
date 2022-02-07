2bffc3168c0d6ee9d1c0d9cccbee140a36f496bb
PatxiEH <patxi.hiribarren@gmail.com>
2022-02-07 20:53:04 +0100

    add mobile responsice menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../assets/LUNETIC.png';
 
+import letter from '../assets/letter.png';
+import profil from '../assets/profil.png';
+import CurrentOpticianContext from '../contexts/CurrentOptician';
+
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
-  console.log(showLinks);
+  const { idOptician } = useContext(CurrentOpticianContext);
 
   const handleShowLinks = () => {
     setShowLinks(!showLinks);
@@ -44,6 +48,25 @@ const Navbar = () => {
               <li>News</li>
             </Link>
           </ul>
+          <div className="navbar__contact_profil">
+            <Link to="/contact">
+              <div className="sidebar__letter">
+                <div className="sidebar__box">
+                  <img src={letter} alt="letter" />
+                  <p>Contact</p>
+                </div>
+              </div>
+            </Link>
+
+            <Link to={idOptician == 0 ? '/login' : '/optician-home'}>
+              <div className="sidebar__profil">
+                <div className="sidebar__box">
+                  <img src={profil} alt="profil" />
+                  {idOptician != 0 ? <p>Mon compte</p> : <p>Se connecter</p>}
+                </div>
+              </div>
+            </Link>
+          </div>
         </div>
         <div
           className="navbar__menu-burger"
