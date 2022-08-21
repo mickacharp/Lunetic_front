764e3cd24b7a882bbee66d26a3b7458c9e17c82d
mickacharp <mickaelcharp@gmail.com>
2022-08-21 04:03:17 +0200

    cleaned unused variables of Context

--- src/contexts/CurrentOptician.tsx
+++ src/contexts/CurrentOptician.tsx
@@ -4,8 +4,6 @@ import { useCookies } from 'react-cookie';
 type OpticianContent = {
   idOptician: number;
   setIdOptician: React.Dispatch<React.SetStateAction<number>>;
-  admin: boolean;
-  setAdmin: React.Dispatch<React.SetStateAction<boolean>>;
   logout: () => void;
 };
 
@@ -14,19 +12,15 @@ type Props = { children: JSX.Element };
 const CurrentOpticianContext = createContext<OpticianContent>({
   idOptician: 0,
   setIdOptician: () => {},
-  admin: false,
-  setAdmin: () => {},
   logout: () => {},
 });
 
 export const CurrentOpticianContextProvider: React.FC<Props> = ({ children }) => {
   const [idOptician, setIdOptician] = useState<number>(0);
-  const [admin, setAdmin] = useState<boolean>(false);
   const removeCookie = useCookies(['optician_token'])[2];
 
   const logout = (): void => {
     setIdOptician(0);
-    setAdmin(false);
     removeCookie('optician_token');
   };
 
@@ -36,8 +30,6 @@ export const CurrentOpticianContextProvider: React.FC<Props> = ({ children }) =>
         idOptician,
         setIdOptician,
         logout,
-        admin,
-        setAdmin,
       }}>
       {children}
     </CurrentOpticianContext.Provider>
